528731d88e47ad53d93fa00d243f906f60ea0041
hyunjin <moonjini0602@naver.com>
2019-03-07 16:57:36 +0900

    last blog

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #INA
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
